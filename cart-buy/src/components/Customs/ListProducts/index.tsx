f1695b80f586e9b986285113cd2033c3261478b5
Thiago da Paz <thiagopazjunior@gmail.com>
2023-02-02 22:58:23 -0300

    refactor: In ListProducts component

--- cart-buy/src/components/Customs/ListProducts/index.tsx
+++ cart-buy/src/components/Customs/ListProducts/index.tsx
@@ -1,18 +1,9 @@
-import { useEffect, useState } from "react"
 import { ClipLoader } from "react-spinners"
 
 import { CardProduct } from "../CardProduct"
-import { getAllProducts } from "../../../services/getAllProducts"
-import { Product } from "../../../types"
+import { ListProductsProps } from "./types"
 
-export function ListProducts() {
-  const [products, setProducts] = useState<Product[]>([])
-
-  useEffect(() => {
-    getAllProducts()
-      .then(res => setProducts(res))
-      .catch(error => console.log(error))
-  }, [])
+export function ListProducts({ products }: ListProductsProps) {
 
   return (
     <>
--- cart-buy/src/components/Home/index.tsx
+++ cart-buy/src/components/Home/index.tsx
@@ -1,11 +1,22 @@
-import { ListProducts } from '../Customs/ListProducts'
+import { useState, useEffect } from "react"
+
+import { ListProducts } from "../Customs/ListProducts"
+import { getAllProducts } from "../../services/getAllProducts"
+import { Product } from "../../types"
 
 export function Home() {
+  const [products, setProducts] = useState<Product[]>([])
+
+  useEffect(() => {
+    getAllProducts()
+      .then(res => setProducts(res))
+      .catch(error => console.log(error))
+  }, [])
   
   return (
     <>
       <main>
-        <ListProducts />
+        <ListProducts products={products}/>
       </main>
     </>
   )
