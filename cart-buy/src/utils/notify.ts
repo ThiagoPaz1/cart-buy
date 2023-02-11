3e32f1ba84451cd21ed90bf8b7f15888b6ef3f1b
Thiago da Paz <thiagopazjunior@gmail.com>
2023-02-11 15:00:46 -0300

    refactor

--- cart-buy/src/utils/notify.ts
+++ cart-buy/src/utils/notify.ts
@@ -3,7 +3,8 @@ import { toast } from "react-toastify"
 export function notify(msg: string, type: string) {
   if (type === "success") {
     toast.success(msg, {
-      position: toast.POSITION.TOP_CENTER
+      autoClose: 3000,
+      position: toast.POSITION.TOP_CENTER,
     })
   }
 }
